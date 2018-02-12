0a531c634fb54d250150f862f7e90b0a06243635
zwangbv <zwangbv@csl4wk62.win2k.cs.ust.hk>
2018-02-12 16:25:05 +0800

    Added a congratulation message

--- src/labs/comp3111/ust/hk/lab1.java
+++ src/labs/comp3111/ust/hk/lab1.java
@@ -3,5 +3,6 @@ package labs.comp3111.ust.hk;
 public class lab1 {
 	public static void main(String[] args){
 		System.out.println("Lab 1: Introduction to GIt and GItHub");
+		System.out.println("Congratualation! You learned the Git Basics");
 	}
 }
